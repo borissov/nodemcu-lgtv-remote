89bfbdb8b406f5cf244359d35ac74083b55944d8
Radoslav Borissov <radoslav.borissov@gmail.com>
2019-11-21 09:36:43 +0200

    Fix for mobile browser.

--- remote.ino
+++ remote.ino
@@ -64,10 +64,15 @@ String indexPage()
         "<script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js\" integrity=\"sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM\" crossorigin=\"anonymous\"></script>"
         "<script>" 
         " var clickTimer; "
-        " function sendData(id){ " 
+        " function sendData(id, event){ " 
             " $.ajax({url:'/send/'+id})" 
             " .done((result) => { if(id === result['command']){  console.log('done'); }else{ console.log(result); } }); " 
             " clickTimer = window.setTimeout(sendData.bind(this, id), 200); "
+            " if (typeof event !== 'undefined'){ "
+                " event.preventDefault(); "
+                " event.stopPropagation(); "
+            " } "
+            " return false; " 
         " } " 
         " function stopData(){ "
             " if(clickTimer) window.clearTimeout(clickTimer); "
@@ -86,48 +91,48 @@ String indexPage()
         "<form>"
             "<div class=\"form-group\">"
                 "<div class=\"btn-group\">"
-                    "<button ontouchstart=\"sendData(551494620)\" onmousedown=\"sendData(551494620)\" type=\"button\" class=\"btn btn-warning\" >ON</button>"
-                    "<button ontouchstart=\"sendData(551527260)\" onmousedown=\"sendData(551527260)\" type=\"button\" class=\"btn btn-secondary\" >OFF</button>"
-                    "<button ontouchstart=\"sendData(551516295)\" onmousedown=\"sendData(551516295)\" type=\"button\" class=\"btn btn-success\" >FAV</button>"
-                    "<button ontouchstart=\"sendData(551489520)\" onmousedown=\"sendData(551489520)\" type=\"button\" class=\"btn btn-success\" >INP</button>"
+                    "<button ontouchstart=\"sendData(551494620,event)\" onmousedown=\"sendData(551494620,event)\" type=\"button\" class=\"btn btn-warning\" >ON</button>"
+                    "<button ontouchstart=\"sendData(551527260,event)\" onmousedown=\"sendData(551527260,event)\" type=\"button\" class=\"btn btn-secondary\" >OFF</button>"
+                    "<button ontouchstart=\"sendData(551516295,event)\" onmousedown=\"sendData(551516295,event)\" type=\"button\" class=\"btn btn-success\" >FAV</button>"
+                    "<button ontouchstart=\"sendData(551489520,event)\" onmousedown=\"sendData(551489520,event)\" type=\"button\" class=\"btn btn-success\" >INP</button>"
                 "</div>"
             "</div>"
             "<div class=\"form-group\">"
                 "<div class=\"btn-group-vertical\" role=\"group\" >"
                     "<div class=\"btn-group\">"
-                        "<button ontouchstart=\"sendData(551520375)\" onmousedown=\"sendData(551520375)\" type=\"button\" class=\"btn btn-light\" >1</button>"
-                        "<button ontouchstart=\"sendData(551504055)\" onmousedown=\"sendData(551504055)\" type=\"button\" class=\"btn btn-light\" >2</button>"
-                        "<button ontouchstart=\"sendData(551536695)\" onmousedown=\"sendData(551536695)\" type=\"button\" class=\"btn btn-light\" >3</button>"
-                        "<button ontouchstart=\"sendData(551495895)\" onmousedown=\"sendData(551495895)\" type=\"button\" class=\"btn btn-light\" >4</button>"
+                        "<button ontouchstart=\"sendData(551520375,event)\" onmousedown=\"sendData(551520375,event)\" type=\"button\" class=\"btn btn-light\" >1</button>"
+                        "<button ontouchstart=\"sendData(551504055,event)\" onmousedown=\"sendData(551504055,event)\" type=\"button\" class=\"btn btn-light\" >2</button>"
+                        "<button ontouchstart=\"sendData(551536695,event)\" onmousedown=\"sendData(551536695,event)\" type=\"button\" class=\"btn btn-light\" >3</button>"
+                        "<button ontouchstart=\"sendData(551495895,event)\" onmousedown=\"sendData(551495895,event)\" type=\"button\" class=\"btn btn-light\" >4</button>"
                     "</div>"
                     "<div class=\"btn-group\">"
-                        "<button ontouchstart=\"sendData(551528535)\" onmousedown=\"sendData(551528535)\" type=\"button\" class=\"btn btn-light\" >5</button>"
-                        "<button ontouchstart=\"sendData(551512215)\" onmousedown=\"sendData(551512215)\" type=\"button\" class=\"btn btn-light\" >6</button>"
-                        "<button ontouchstart=\"sendData(551544855)\" onmousedown=\"sendData(551544855)\" type=\"button\" class=\"btn btn-light\" >7</button>"
-                        "<button ontouchstart=\"sendData(551491815)\" onmousedown=\"sendData(551491815)\" type=\"button\" class=\"btn btn-light\" >8</button>"
+                        "<button ontouchstart=\"sendData(551528535,event)\" onmousedown=\"sendData(551528535,event)\" type=\"button\" class=\"btn btn-light\" >5</button>"
+                        "<button ontouchstart=\"sendData(551512215,event)\" onmousedown=\"sendData(551512215,event)\" type=\"button\" class=\"btn btn-light\" >6</button>"
+                        "<button ontouchstart=\"sendData(551544855,event)\" onmousedown=\"sendData(551544855,event)\" type=\"button\" class=\"btn btn-light\" >7</button>"
+                        "<button ontouchstart=\"sendData(551491815,event)\" onmousedown=\"sendData(551491815,event)\" type=\"button\" class=\"btn btn-light\" >8</button>"
                     "</div>"
                     "<div class=\"btn-group\">"
-                        "<button ontouchstart=\"sendData(551524455)\" onmousedown=\"sendData(551524455)\" type=\"button\" class=\"btn btn-light\" >9</button>"
-                        "<button ontouchstart=\"sendData(551487735)\" onmousedown=\"sendData(551487735)\" type=\"button\" class=\"btn btn-light\" >0</button>"
-                        "<button ontouchstart=\"sendData(551537205)\" onmousedown=\"sendData(551537205)\" type=\"button\" class=\"btn btn-secondary\" >list</button>"
-                        "<button ontouchstart=\"sendData(551508135)\" onmousedown=\"sendData(551508135)\" type=\"button\" class=\"btn btn-secondary\" >q.view</button>"
+                        "<button ontouchstart=\"sendData(551524455,event)\" onmousedown=\"sendData(551524455,event)\" type=\"button\" class=\"btn btn-light\" >9</button>"
+                        "<button ontouchstart=\"sendData(551487735,event)\" onmousedown=\"sendData(551487735,event)\" type=\"button\" class=\"btn btn-light\" >0</button>"
+                        "<button ontouchstart=\"sendData(551537205,event)\" onmousedown=\"sendData(551537205,event)\" type=\"button\" class=\"btn btn-secondary\" >list</button>"
+                        "<button ontouchstart=\"sendData(551508135,event)\" onmousedown=\"sendData(551508135,event)\" type=\"button\" class=\"btn btn-secondary\" >q.view</button>"
                     "</div>"
                 "</div>"
             "</div>"
             "<div class=\"form-group\">"
                 "<div class=\"btn-group\">"
-                    "<button ontouchstart=\"sendData(551502015)\" onmousedown=\"sendData(551502015)\" type=\"button\" class=\"btn btn-danger\" >VOL+</button>"
-                    "<button ontouchstart=\"sendData(551534655)\" onmousedown=\"sendData(551534655)\" type=\"button\" class=\"btn btn-light\" >VOL-</button>"
-                    "<button ontouchstart=\"sendData(551485695)\" onmousedown=\"sendData(551485695)\" type=\"button\" class=\"btn btn-danger\" >CH+</button>"
-                    "<button ontouchstart=\"sendData(551518335)\" onmousedown=\"sendData(551518335)\" type=\"button\" class=\"btn btn-light\" >CH-</button>"
+                    "<button ontouchstart=\"sendData(551502015,event)\" onmousedown=\"sendData(551502015,event)\" type=\"button\" class=\"btn btn-danger\" >VOL+</button>"
+                    "<button ontouchstart=\"sendData(551534655,event)\" onmousedown=\"sendData(551534655,event)\" type=\"button\" class=\"btn btn-light\" >VOL-</button>"
+                    "<button ontouchstart=\"sendData(551485695,event)\" onmousedown=\"sendData(551485695,event)\" type=\"button\" class=\"btn btn-danger\" >CH+</button>"
+                    "<button ontouchstart=\"sendData(551518335,event)\" onmousedown=\"sendData(551518335,event)\" type=\"button\" class=\"btn btn-light\" >CH-</button>"
                 "</div>"
             "</div>"
             "<div class=\"form-group\">"
                 "<div class=\"btn-group\">"
-                    "<button ontouchstart=\"sendData(551522415)\" onmousedown=\"sendData(551522415)\" type=\"button\" class=\"btn btn-danger\" >mute</button>"
-                    "<button ontouchstart=\"sendData(551541285)\" onmousedown=\"sendData(551541285)\" type=\"button\" class=\"btn btn-warning\" >exit</button>"
-                    "<button ontouchstart=\"sendData(551490795)\" onmousedown=\"sendData(551490795)\" type=\"button\" class=\"btn btn-warning\" >back</button>"
-                    "<button ontouchstart=\"sendData(551507370)\" onmousedown=\"sendData(551507370)\" type=\"button\" class=\"btn btn-warning\" >info</button>"
+                    "<button ontouchstart=\"sendData(551522415,event)\" onmousedown=\"sendData(551522415,event)\" type=\"button\" class=\"btn btn-danger\" >mute</button>"
+                    "<button ontouchstart=\"sendData(551541285,event)\" onmousedown=\"sendData(551541285,event)\" type=\"button\" class=\"btn btn-warning\" >exit</button>"
+                    "<button ontouchstart=\"sendData(551490795,event)\" onmousedown=\"sendData(551490795,event)\" type=\"button\" class=\"btn btn-warning\" >back</button>"
+                    "<button ontouchstart=\"sendData(551507370,event)\" onmousedown=\"sendData(551507370,event)\" type=\"button\" class=\"btn btn-warning\" >info</button>"
                 "</div>"
             "</div>"
         "</form>"
@@ -172,18 +177,14 @@ void loop()
           //index page
           client.println(indexPage());
           client.stop();
-        }
-        else if (request.indexOf("GET /favicon.ico HTTP/1.1") != -1)  
-        {
-        
+          return;
         }
         else if (request.indexOf("send") != -1)  
         {
           //response page
           request.replace(" HTTP/1.1", "");
           request.replace("GET /send/", "");
-    
-        
+     
           irsend.sendNEC(request.toInt(), 32);
           delay(50);
 
@@ -191,8 +192,12 @@ void loop()
           client.stop();
 
           return;
-
         }
+        else if (request.indexOf("GET /favicon.ico HTTP/1.1") != -1)  
+        {
+        
+        }
+
         else
         {
           client.println("HTTP/1.1 404 Not Found\n");
